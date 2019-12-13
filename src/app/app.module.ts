d4c907145356bc8c78ad515cb1f539f004539698
marielysis <marielys11@yahoo.com>
2019-12-13 15:59:30 -0300

    Creado componentes del proyecto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,17 +2,28 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './components/header/header.component';
-import { BodyComponent } from './components/body/body.component';
 import { FooterComponent } from './components/footer/footer.component';
+import { HomeComponent } from './components/home/home.component';
+import { WaiterComponent } from './components/waiter/waiter.component';
+import { TablesComponent } from './components/tables/tables.component';
+import { MenuComponent } from './components/menu/menu.component';
+import { BreakfastComponent } from './components/breakfast/breakfast.component';
+import { LunchComponent } from './components/lunch/lunch.component';
+
+
+
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent,
-    BodyComponent,
-    FooterComponent
+    FooterComponent,
+    HomeComponent,
+    WaiterComponent,
+    TablesComponent,
+    MenuComponent,
+    BreakfastComponent,
+    LunchComponent
   ],
   imports: [
     BrowserModule
