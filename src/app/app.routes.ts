f0733aa41a85a1a462ba6a95dcef8edecbc2a5f7
marielysis <marielys11@yahoo.com>
2019-12-14 01:21:09 -0300

    Archivo de rutas funcionando

---
+++ src/app/app.routes.ts
@@ -0,0 +1,23 @@
+import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { WaiterComponent } from './components/waiter/waiter.component';
+import { TablesComponent } from './components/tables/tables.component';
+import { MenuComponent } from './components/menu/menu.component';
+import { BreakfastComponent } from './components/breakfast/breakfast.component';
+import { LunchComponent } from './components/lunch/lunch.component';
+
+
+
+
+const APP_ROUTES: Routes = [
+    { path: 'home', component: HomeComponent },
+    { path: 'waiter', component: WaiterComponent },
+    { path: 'tables', component: TablesComponent},
+    { path: 'menu', component: MenuComponent},
+    { path: 'breakfast', component: BreakfastComponent},
+    { path: 'lunch', component: LunchComponent},
+  ];
+
+export const APP_ROUTING = RouterModule.forRoot(APP_ROUTES);
+
+
