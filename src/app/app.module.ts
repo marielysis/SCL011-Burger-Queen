79b7707ceae862fcf36e3e81bd8be22f94bb980d
Mariajose <mariajosegomezcampo@gmail.com>
2019-12-21 17:43:28 -0500

    resolbiendo error y ya la ruta esta exacta

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -21,7 +21,7 @@ import { MenuComponent } from './components/menu/menu.component';
 import { BreakfastComponent } from './components/breakfast/breakfast.component';
 import { LunchComponent } from './components/lunch/lunch.component';
 import { NavBarComponent } from './components/nav-bar/nav-bar.component';
-import { KitchenComponent } from './kitchen/kitchen.component';
+import { KitchenComponent } from './components/kitchen/kitchen.component';
 
 
 @NgModule({
