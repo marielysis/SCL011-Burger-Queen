ac3e9c58c10d110b5082fcd0d1cd983a697501a0
marielysis <53873085+marielysis@users.noreply.github.com>
2020-01-03 23:19:15 -0300

    Agregando formularios configurar menu para llenar la base de datos desde app, agregando fromulario para editar y actualizar un producto del menu, funcion eliminar un producto del menu (#21)
    Co-authored-by: SusejR <57444604+SusejR@users.noreply.github.com>

--- src/app/servicios/orden.service.ts
+++ src/app/servicios/orden.service.ts
@@ -1,24 +1,58 @@
 import { Injectable } from '@angular/core';
 import products from '../../assets/data/products.json';
+import{ AngularFirestore, AngularFirestoreCollection, AngularFirestoreDocument} from '@angular/fire/firestore';
+import { Observable } from 'rxjs';//modulo de angular que nos devuelve los datos 
+import { Product } from '../models/product';
 import { map, filter } from 'rxjs/operators';
+
 
 @Injectable ()
 export class OrdenService {
-
+  // Variable para guardar data de json
     private orden: any[] = [{ products
     }];
 
+  productsCollection: AngularFirestoreCollection<Product>;
+  products: Observable<Product[]>;
+  productDoc: AngularFirestoreDocument<Product>;
 
-    constructor() {
-        console.log('Servicio listo para usar!!!');
+
+    constructor(public db: AngularFirestore){
+      // this.products = this.db.collection('products').valueChanges();
+      this.productsCollection = this.db.collection('products');
+      this.products = this.productsCollection.snapshotChanges().pipe(map(actions => { 
+        return actions.map(a => {
+          const data = a.payload.doc.data() as Product;
+          data.id = a.payload.doc.id;
+          return data;
+        }); 
+      }));
+    }
+    // Retorna los productos desde base de datos firestore
+    getProducts() {
+      return this.products;
     }
 
+    addProducts(product: Product) {
+      return this.productsCollection.add(product);
+    }  
     // filtramos la data del json para que solo me muestre los desayunos
     getOrdenes() {
-    return this.orden[0].products.filter((element: any) => element.type === 'desayuno');
+    return this.orden[0].products.filter((element: any) => element.type === 'almuerzo');
+    }
+    // usamos map para crear un arreglo con los precios de los productos de data json
+    getProductss() {
+        return this.orden[0].products.map((elementProduct: any ) => elementProduct.price);
+        }
+
+
+    deleteProduct(product: Product) {
+      this.productDoc = this.db.doc(`products/${product.id}`);
+      this.productDoc.delete();
+    }
+    updateProduct(product: Product) {
+      this.productDoc = this.db.doc(`products/${product.id}`);
+      this.productDoc.update(product);
     }
 }
-
-
-
 
