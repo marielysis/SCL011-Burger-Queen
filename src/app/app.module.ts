847e466eec05cf3111a2e8c00b609d87d564fc66
SusejR <rodriguezbsusej@gmail.com>
2020-01-07 08:37:24 -0300

    Tomar orden listo desde menu almuerzo con datos de firebase, toma orden y la envia a cocina

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -16,13 +16,15 @@ import { OrdenTwoService } from './servicios/ordenTwo.service';
 import { AppComponent } from './app.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { HomeComponent } from './components/home/home.component';
-import { WaiterComponent } from './components/waiter/waiter.component';
+
 import { TablesComponent } from './components/tables/tables.component';
 import { MenuComponent } from './components/menu/menu.component';
 import { BreakfastComponent } from './components/breakfast/breakfast.component';
 import { LunchComponent } from './components/lunch/lunch.component';
 import { NavBarComponent } from './components/nav-bar/nav-bar.component';
 import { KitchenComponent } from './components/kitchen/kitchen.component';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
 
 //firebase
 import { AngularFireModule } from '@angular/fire';// para conectarnos a los servicios de firibase
@@ -30,7 +32,8 @@ import {AngularFirestoreModule} from '@angular/fire/firestore';
 import { AngularFireStorageModule } from '@angular/fire/storage';
 
 // environment config
-import {environment} from '../environments/environment';// configuracion a donde me voy a conectar 
+import {environment} from '../environments/environment';
+// configuracion a donde me voy a conectar 
 
 
 @NgModule({
@@ -38,13 +41,14 @@ import {environment} from '../environments/environment';// configuracion a donde
     AppComponent,
     FooterComponent,
     HomeComponent,
-    WaiterComponent,
     TablesComponent,
     MenuComponent,
     BreakfastComponent,
     LunchComponent,
     NavBarComponent,
     KitchenComponent,
+    LoginComponent,
+    RegisterComponent,
   
   ],
 
