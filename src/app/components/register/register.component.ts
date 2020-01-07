65387bad554631e07b2b8e18aa5826b58e346a04
Mariajose <mariajosegomezcampo@gmail.com>
2020-01-07 10:54:21 -0500

    imagen sube a firebase storage

--- src/app/components/register/register.component.ts
+++ src/app/components/register/register.component.ts
@@ -13,17 +13,27 @@ import { Observable } from 'rxjs/internal/Observable';
 })
 export class RegisterComponent implements OnInit {
 
-  constructor(private router: Router, private authService: AuthService) { }
+  constructor(private router: Router, private authService: AuthService, private storage: AngularFireStorage) { }
   public email: string = '';
   public password: string = '';
 
+
+ uploadPercent: Observable<number>;
+  urlImage: Observable<string>;
 
   ngOnInit() {
   }
 
 
   onUpload(e) {
-     console.log('subir',e);
+    //  console.log('subir',e);
+     const id = Math.random().toString(36).substring(2);
+     const file = e.target.files[0];
+     const filePath = `uploads/profile_${id}`;
+     const ref = this.storage.ref(filePath);
+     const task = this.storage.upload(filePath, file);
+     this.uploadPercent = task.percentageChanges();
+     task.snapshotChanges().pipe(finalize(() => this.urlImage = ref.getDownloadURL())).subscribe();
   }
   
   onAddUser() {
